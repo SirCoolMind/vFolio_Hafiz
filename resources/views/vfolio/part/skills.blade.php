26a98537e6427e5ba3ce7bb709df49a927dd961d
SirCoolMind <90669475+hafizunijaya@users.noreply.github.com>
2023-06-17 09:41:10 +0800

    Updated logo of skills

--- resources/views/vfolio/part/skills.blade.php
+++ resources/views/vfolio/part/skills.blade.php
@@ -1,116 +1,63 @@
+<style>
+    .customHeight-1000{
+        height: 50px !important;
+    }
+</style>
 <div class="container py-5">
     <h1 class="text-center fw-normal wow fadeIn"> Skills</h1>
     <div class="row py-3">
         <div class="col-md-3">
             <div class="px-lg-3">
                 <h4 class="wow fadeInUp">Front-End</h4>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">React JS</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 86%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">86%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/laravel.svg') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Laravel Blade</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Laravel Blade</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">80%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/htmlcssjs.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;HTML + CSS + JS</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">HTML + CSS</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">100%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/sass.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;SASS</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Javascript</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">100%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/react.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;React JS</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">SASS</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">90%</div>
-                    </div>
-                </div>
+
             </div>
         </div>
         <div class="col-md-3">
             <div class="px-lg-3">
                 <h4 class="wow fadeInUp">Back-End</h4>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">PHP</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 92%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">92%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/php.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;PHP</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Laravel Framework</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 99%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">99%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/laravel.svg') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Laravel Framework</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Node JS</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 79%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">79%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/nodejs.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Node JS</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">MySQL</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 79%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">79%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/mysql.svg') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;MySQL</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">MongoDB</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 79%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">79%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/mongo.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;MongoDB</span>
                 </div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="px-lg-3">
                 <h4 class="wow fadeInUp">Tools</h4>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Laragon</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 92%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">92%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/laragon.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Laragon</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Git (GitHub Desktop)</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 99%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">99%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/githubdesktop.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Git (GitHub Desktop)</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Windows</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 79%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">79%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/window.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Windows (10)</span>
                 </div>
-                <div class="progress-wrapper wow fadeInUp">
-                    <span class="caption">Linux</span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 79%;" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100">79%</div>
-                    </div>
+                <div class="progress-wrapper wow fadeInUp customHeight-1000">
+                    <span class="caption"> <img src="{{ asset('assets/img/logo/linux.png') }}" height="auto;" width="50px;">&nbsp;&nbsp;&nbsp;Linux (Ubuntu)</span>
                 </div>
             </div>
         </div>
