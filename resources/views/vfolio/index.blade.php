1d64cb1672214e674b3a03bbb2c70ac952ed30e1
SirCoolMind <90669475+hafizunijaya@users.noreply.github.com>
2023-06-18 09:32:23 +0800

    updated title of website

--- resources/views/vfolio/index.blade.php
+++ resources/views/vfolio/index.blade.php
@@ -7,7 +7,7 @@
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Virtual Folio - Portfolio HTML5 Template</title>
+    <title>Virtual Folio - Muhammad Hafiz Ruslan</title>
 
     <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon">
 
